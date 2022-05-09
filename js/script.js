660ed646084b4ec6d9bee46ac63adced547b7f91
matte887 <m.erroi92@gmail.com>
2022-05-09 16:52:05 +0200

    milestone 3: work in progress

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const app = new Vue(
     {
         el: '#root',
         data: {
+            newToDo: {
+                text: '',
+                done: false
+            },
             toDoList: [
                 {
                     text: 'Portare fuori cane',
@@ -18,13 +22,18 @@ const app = new Vue(
                 {
                     text: 'Comprare una Ferrari',
                     done: false
-                },
+                }
             ]
         },
         methods: {
             removeToDo: function(index) {
                 this.toDoList.splice(index, 1);
                 console.log(index);
+            },
+            addToDoElement: function() {
+                this.toDoList.push(this.newToDo);
+                // this.newToDo.text= '';
+                console.log(this.newToDo);
             }
         }
     }
